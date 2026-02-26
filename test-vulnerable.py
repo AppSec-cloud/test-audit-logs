ba8d9ea896612a04a648f39cd4e2f3b1ccb9dfd7
mayshavit <mayshavit1234@gmail.com>
2026-02-26 13:26:04 +0200

    Update test-vulnerable.py

--- test-vulnerable.py
+++ test-vulnerable.py
@@ -5,6 +5,6 @@ app = flask.Flask(__name__)
 
 @app.route("/run")
 def run():
-    # Dangerous: directly injecting user input into a shell command!
+    # Dangerous: directly injecting user input into a shell command!!
     command = flask.request.args.get("command")
     os.system("echo " + command)
